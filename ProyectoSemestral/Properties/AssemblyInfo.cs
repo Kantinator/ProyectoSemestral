61c5684b386c064b101b02c4249aa28dc5e57a15
kamlu <kamlu@DESKTOP-6EBHSTK>
2024-11-18 23:17:51 -0500

    Add project files.

---
+++ ProyectoSemestral/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProyectoSemestral")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProyectoSemestral")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a06905c8-0c7b-4ef4-a706-dd96e2144ca3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProyectoSemestral/bd/Conexion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemestral.bd
+{
+    internal class Conexion
+    {
+        private static SqlConnection conexion;
+
+        public static SqlConnection getConexion()
+        {
+            if (conexion != null)
+            {
+                return conexion;
+
+            }
+            conexion = new SqlConnection();
+            conexion.ConnectionString = "Data Source = DESKTOP-ISIP9K7\\;Initial Catalog=GimnasioDB;Integrated Security = True";
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Error de Conexion" + ex.Message);
+                return null;
+
+            }
+        }
+        public static void cerrarConexion()
+        {
+            if (conexion != null)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
