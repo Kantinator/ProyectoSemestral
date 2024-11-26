8346c791c1e459402dfbdbb67d8b4c2b26b7099a
hernandez <alex.hernande3@utp.ac.pa>
2024-11-26 16:45:35 -0500

    login

---
+++ ProyectoSemestral/vista/LoginForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ProyectoSemestral.vista
+{
+    partial class LoginForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Button btnIniciarSesion;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(30, 30);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(55, 15);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(120, 27);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(200, 23);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Location = new System.Drawing.Point(30, 70);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(74, 15);
+            this.lblContrasena.TabIndex = 2;
+            this.lblContrasena.Text = "Contraseña:";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(120, 67);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '*';
+            this.txtContrasena.Size = new System.Drawing.Size(200, 23);
+            this.txtContrasena.TabIndex = 3;
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.Location = new System.Drawing.Point(120, 110);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(100, 30);
+            this.btnIniciarSesion.TabIndex = 4;
+            this.btnIniciarSesion.Text = "Iniciar Sesión";
+            this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
+            // 
+            // LoginForm
+            // 
+            this.ClientSize = new System.Drawing.Size(350, 180);
+            this.Controls.Add(this.btnIniciarSesion);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.lblContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "LoginForm";
+            this.Text = "Inicio de Sesión";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ ProyectoSemestral/vista/LoginForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using ProyectoSemestral.modelo;
+
+namespace ProyectoSemestral.vista
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contrasena = txtContrasena.Text;
+
+            // Crear una instancia de la clase Autenticacion
+            Autenticacion autenticacion = new Autenticacion();
+
+            if (autenticacion.IniciarSesion(usuario, contrasena))
+            {
+                MessageBox.Show("Inicio de sesión exitoso", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Aquí puedes abrir la ventana principal de la aplicación
+                this.Hide();
+                MainForm mainForm = new MainForm(); // Asegúrate de tener un formulario principal
+                mainForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Credenciales incorrectas. Intente de nuevo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
