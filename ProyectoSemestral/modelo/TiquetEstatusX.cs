744e83e020936471335555d3a28e0337abb88fe6
Fenando Rebolledo <fernandoorebolledo@gmail.com>
2024-11-26 16:07:43 -0500

    Clases Tipos de solicitud, Niveles de prioridad, Apertura de tiquetes, Tiquetes de acuerdo a un estatus X

---
+++ ProyectoSemestral/modelo/TiquetEstatusX.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemestral.modelo
+{
+    public class TiquetEstatusX
+    {
+        public List<AperturaTiquete> ObtenerTiquetesPorEstatus(int idEstatus)
+        {
+            List<AperturaTiquete> tiquetes = new List<AperturaTiquete>();
+
+            using (SqlConnection connection = new SqlConnection("tu_cadena_de_conexion"))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Tiquetes WHERE ID_Estatus = @IdEstatus";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@IdEstatus", idEstatus);
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    AperturaTiquete tiquete = new AperturaTiquete(
+                        (int)reader["ID_TipoSolicitud"],
+                        (string)reader["DescripcionSolicitud"],
+                        (int)reader["ID_Prioridad"],
+                        (string)reader["NombreUsuario"],
+                        (string)reader["AreaTrabajo"],
+                        (int)reader["ID_Estatus"]
+                    );
+                    tiquetes.Add(tiquete);
+                }
+            }
+            return tiquetes;
+        }
+    }
+}
